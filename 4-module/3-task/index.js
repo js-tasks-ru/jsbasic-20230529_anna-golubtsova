0ca3e19539991478444b415a7914bd23450e9561
Anna Golubtsova <marickam@yandex.ru>
2023-06-20 00:41:12 +0300

    added functions for converting an array into a standard HTML list, selecting cells diagonally, making data changes

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,29 @@
 function highlight(table) {
-  // ваш код...
+  for (let tr of table.children[1].rows) {
+   let available = tr.cells[3];
+   
+    if(available.hasAttribute('data-available')) {
+      console.log(typeof(available.dataset.available));
+      if(available.dataset.available === 'true') {
+        tr.classList.add('available');
+      } else if(available.dataset.available === 'false') {
+        tr.classList.add('unavailable');
+      }
+    } else {
+      tr.hidden = true;
+    }
+  
+    let gender = tr.cells[2].textContent;
+    if(gender === 'm') {
+      tr.classList.add('male');
+    } else {
+      tr.classList.add("female");
+    }
+  
+     let age = +tr.cells[1].textContent;
+     if(age < 18) {
+      tr.style['text-decoration'] = 'line-through';
+     }
+  }   
 }
+
